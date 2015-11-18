259f5254d5262bfbbbd4175af1c67adbc6c31493
Kevin Wang <kevinwang@kevinwang.us>
2015-11-18 02:01:33 -0800

    Added placeholder tokens and projectoxford installation instructions

---
+++ FlaskWebProject/tokens.py
@@ -0,0 +1,5 @@
+"""enter the tokens that you get from https://datamarket.azure.com/account
+and https://www.projectoxford.ai/ here"""
+bing_token = "ENTER YOUR BING TOKEN HERE"
+bing_customer_id = "ENTER YOUR BING CUSTOMER ID HERE"
+oxford_computer_vision = "ENTER YOUR OXFORD TOKEN HERE"
